d53cfaa1151f3db13157f5e05633f7e3abe58af5
Lukasz159 <zimo1212@gmail.com>
2022-05-16 17:14:27 +0200

    Modele danych
    Samochód, Typ samochodu, Wypożyczenie, Status wypożyczenia

---
+++ CarRentalApp/CarRentalApp/Models/StatusWypozyczenia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Models
+{
+    public enum StatusWypozyczenia
+    {
+        Rezerwacja,
+        Aktywne,
+        Zakonczone
+    }
+}
---
+++ CarRentalApp/CarRentalApp/Models/TypSamochodu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Models
+{
+    public class TypSamochodu
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public ICollection<Samochod> Samochody { get; set; }
+    }
+}
